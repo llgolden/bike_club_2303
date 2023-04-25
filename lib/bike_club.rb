dd036ff18ce8b07f33a186d3dd087b85314b33e7
Lauren Golden <llgolden@gmail.com>
2023-04-25 10:20:56 -0600

    Bike Club setup

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -11,26 +11,32 @@ class BikeClub
     @bikers << biker
   end
 
-  # def most_rides
+  def most_rides
 
-  # end
+  end
+# A `BikeClub` can tell us which Biker has logged the most rides.
+
+
 
   # def best_time(ride)
 
   # end
+# A `BikeClub` can tell us which Biker has the best time for a given Ride.
+
+
 
   # def bikers_eligible(ride)
 
   # end
+
+# A `BikeClub` can tell us which Bikers are eligible for a given Ride. A Biker is eligible for a Ride if the terrain is acceptable to them and the total distance does not exceed their max distance.
 
 end #final
 
 
 # | Method Name | Return Value |
 # | ----------- | ------------ |
-# | `name` | Club name as a `String` |
-# | `bikers` | `Array` of `Biker` objects |
-# | `add_biker(biker)` | `Array` of `Biker` objects |
+
 # | `most_rides` | `Biker` object |
 # | `best_time(ride)` | `Biker` object |
 # | `bikers_eligible(ride)` | `Array` of `Biker` objects |
--- spec/bike_club.rb
+++ spec/bike_club.rb
@@ -14,8 +14,44 @@ RSpec.describe BikeClub do
     expect(drinkers.name).to eq("Drinkers with a Biking Problem")
     expect(drinkers.bikers).to eq([])
   end
-end #final
 
-# expect().to eq()
-# expect().to eq()
-# expect().to eq()
+  it "can add new members" do
+    drinkers = BikeClub.new("Drinkers with a Biking Problem")
+    biker1 = Biker.new("Kenny", 30) 
+    biker2 = Biker.new("Athena", 25)
+    expect(drinkers.bikers).to eq([])
+    drinkers.add_biker(biker1)
+    drinkers.add_biker(biker2)
+    expect(drinkers.bikers).to eq([biker1, biker2])
+  end
+
+  it "can tell us which Biker has logged the most rides" do
+    drinkers = BikeClub.new("Drinkers with a Biking Problem")
+    biker1 = Biker.new("Kenny", 30) 
+    biker2 = Biker.new("Athena", 25)
+    expect().to eq()
+    expect().to eq()
+    expect().to eq()
+    expect().to eq()
+
+  end
+
+  xit "can tell us which Biker has the best time for a given Ride" do
+    drinkers = BikeClub.new("Drinkers with a Biking Problem")
+    ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+    biker1 = Biker.new("Kenny", 30) 
+    biker2 = Biker.new("Athena", 25)
+    expect().to eq()
+  end
+
+  xit "can tell us which Bikers are eligible for a given Ride" do
+    drinkers = BikeClub.new("Drinkers with a Biking Problem")
+    ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+    biker1 = Biker.new("Kenny", 30) 
+    biker2 = Biker.new("Athena", 25)
+    expect().to eq()
+  end
+
+end #final
