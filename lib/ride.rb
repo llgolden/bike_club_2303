85a7e2cb386f21fba8c9e30c6d6a830893e2d207
Lauren Golden <llgolden@gmail.com>
2023-04-25 09:11:38 -0600

    Completed Iteration 1

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,23 @@
 class Ride
+  attr_reader :name, :distance, :loop, :terrain
+            
+  def initialize(details)
+    @name = details[:name]
+    @distance = details[:distance]
+    @loop = details[:loop]
+    @terrain = details[:terrain]
+  end
 
-end
+  def loop? 
+    @loop
+  end
+
+  def total_distance
+    if @loop == true
+      @distance
+    else
+      @distance * 2
+    end
+  end
+  
+end #final
